cf7289f47776cf310396b2f5a79f6d6bb3455cfd
Sarvesh Kumar (iDEAS-Cloud Transformation) <sa20202906@wipro.com>
2021-09-02 23:08:55 +0530

    Added Servlet

---
+++ src/main/java/com/wipro/loginValidation/validate.java
@@ -0,0 +1,34 @@
+package com.wipro.loginValidation;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class validate extends HttpServlet {
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String name=request.getParameter("name");
+		String password=request.getParameter("password");
+		
+		PrintWriter out=response.getWriter();
+		
+		if(name.equals("admin") && password.equals("Admin"))
+		{
+			out.print("Welcome admin...");
+		}
+		else
+		{
+			request.getRequestDispatcher("index.jsp").include(request,response);
+		}
+		
+	}
+
+	
+
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,10 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+<h2>Hello there...</h2>
+<form action="validate" method="post">
+Username : <input type="text" name="name"/><br/>
+Password : <input type="password" name="password"/><br/>
+<input type="submit" value="Login"> 
+</form>
 </body>
 </html>
